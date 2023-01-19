f067c4b1cbee73b170beb1f8e1178e6b27431b02
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-01-19 18:30:31 +0000

    Deploying to gh-pages from @ Malakhow-Alexandr/for-local-training@b615a47df574d815c314f5d0ed578106db122fa0 🚀

--- index.059c7e9d.js
+++
@@ -1,2 +0,0 @@
-function r(r){return fetch(`http://localhost:3000/video-cards/${r}`).then((r=>{if(!r.ok)throw new Error;return r.json()}))}fetch("http://localhost:3000/video-cards").then((r=>{if(!r.ok)throw new Error;return r.json})),r(2),r(4);
-//# sourceMappingURL=index.059c7e9d.js.map
--- index.9a332155.js
+++
@@ -1,2 +0,0 @@
-!function(){var n="http://localhost:3000";function o(o){return fetch("".concat(n,"/video-cards/").concat(o)).then((function(n){if(!n.ok)throw new Error;return n.json()}))}fetch("".concat(n,"/video-cards")).then((function(n){if(!n.ok)throw new Error;return n.json})),o(2),o(4)}();
-//# sourceMappingURL=index.9a332155.js.map
